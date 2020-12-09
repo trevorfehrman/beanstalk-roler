9d05482b89c17852fd53df5c529ba88302635b88
Trevor Fehrman <trevorfehrman@gmail.com>
2020-12-08 18:04:12 -0800

    cloud function for user doc creation working, session invite skeleton up and running

--- src/screens/session-browser.tsx
+++ src/screens/session-browser.tsx
@@ -3,14 +3,50 @@ import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire'
 import { User } from 'firebase/app'
 import { Link } from 'react-router-dom'
 
-import { ButtonGroup, Button, Text } from '@chakra-ui/react'
+import { Formik, useFormik } from 'formik'
+
+import {
+  ButtonGroup,
+  Button,
+  Input,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 import { ISession } from 'interfaces-and-types/session.interface'
 
 const SessionBrowser: React.FC = () => {
+  const [uids, setUids] = React.useState<string[]>([])
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    onSubmit: values => addSession(values.name),
+  })
+
   const user: User = useUser()
-  const userSessionsQuery = useFirestore().collection('sessions').where('userIds', 'array-contains', user.uid)
+  const userSessionsRef = useFirestore().collection('sessions')
+  const usersRef = useFirestore().collection('users')
+  const users: User[] = useFirestoreCollectionData(usersRef)
+  const userSessionsQuery = userSessionsRef.where('userIds', 'array-contains', user.uid)
   const userSessions: ISession[] = useFirestoreCollectionData(userSessionsQuery, { idField: 'docId' })
+
+  function handleAddToSession(uid: string) {
+    uids.includes(uid) ? setUids(uids.filter(uid => !uid)) : setUids([...uids, uid])
+  }
+
+  function addSession(name: string) {
+    userSessionsRef.add({ name, userIds: [...uids, user.uid] })
+  }
 
   return (
     <div>
@@ -21,6 +57,45 @@ const SessionBrowser: React.FC = () => {
             <Button>{session.name}</Button>
           </Link>
         ))}
+        <Button onClick={onOpen}>Create New Session +</Button>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Create New Session</ModalHeader>
+            <ModalCloseButton />
+            <form onSubmit={formik.handleSubmit}>
+              <ModalBody>
+                <Input id="name" type="text" {...formik.getFieldProps('name')} />
+                <ButtonGroup>
+                  {users.map(otherUser => {
+                    if (otherUser.uid !== user.uid) {
+                      return (
+                        <Button
+                          variant={uids.includes(otherUser.uid) ? 'solid' : 'outline'}
+                          onClick={() => handleAddToSession(otherUser.uid)}
+                          key={user.uid}
+                          colorScheme="teal"
+                        >
+                          {user.displayName}
+                        </Button>
+                      )
+                    }
+                  })}
+                </ButtonGroup>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+                <Button variant="ghost" type="submit">
+                  Create Session +
+                </Button>
+              </ModalFooter>
+            </form>
+          </ModalContent>
+        </Modal>
       </ButtonGroup>
     </div>
   )
